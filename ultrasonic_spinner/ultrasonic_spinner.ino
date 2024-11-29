36c997d93796a84e2bc3b140bb3e7b3e1146cf97
Vladimir Herdman <vovaherdman@gmail.com>
2024-11-29 14:08:52 -0500

    Finished Arduino Code Section

--- ultrasonic_spinner/ultrasonic_spinner.ino
+++ ultrasonic_spinner/ultrasonic_spinner.ino
@@ -1,9 +1,63 @@
+// Servo Portion
+#include <Servo.h>
+const int servoPin = 12;
+Servo myServo;
+
+// Ultrasonic Sensor Portion
+const int trigPin = 9;
+const int echoPin = 10;
+
+float duration;
+float distanceCM;
+
+void calculateDistance(){
+  // Reset ultrasonic sensor
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Send out signal to measure time for return
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculate distance (343 m/s = .034 cm/microsecond)
+  distanceCM = (duration * 0.034) / 2;
+}
+
 void setup() {
-  // put your setup code here, to run once:
-  
+  Serial.begin(9600);
+
+  // Servo
+  myServo.attach(servoPin);
+
+  // Ultrasonic
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Rotate servo from 1 - 90 degrees
+  for (int deg = 1; deg < 181; deg++){
+    myServo.write(deg);
+    delay(30);
+    calculateDistance();
 
+    Serial.print(deg);
+    Serial.print(":");
+    Serial.print(distanceCM);
+    Serial.print("|");
+  }
+
+  // Rotate servo back from 90 - 1 degree
+  for (int deg = 181; deg > 0; deg--){
+    myServo.write(deg);
+    delay(30);
+    calculateDistance();
+
+    Serial.print(deg);
+    Serial.print(":");
+    Serial.print(distanceCM);
+    Serial.print("|");
+  }
 }
